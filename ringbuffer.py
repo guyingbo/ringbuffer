73ac4a13e9a96291df5d0cb0b3454be382e3dbd2
Gu Yingbo <tensiongyb@gmail.com>
2020-08-19 17:09:05 +0800

    rename start/end to head/tail

--- ringbuffer.py
+++ ringbuffer.py
@@ -2,48 +2,48 @@ class OverflowException(Exception):
     pass
 
 
-def _chr_len(x):
+def _chr_len(x: int):
     return len(repr(chr(x))) - 2
 
 
 # RingBuffer structure
 # @: data, -: blank
 # [------------------------------------------] initial state
-#  ^start&end
+#  ^head&tail
 #  ^data
 # [@@@@@@@@----------------------------------] after first push
-#  ^end    ^start
+#  ^tail   ^head
 # [-----@@@@@@@@@----------------------------] after first pull
-#       ^end     ^start
+#       ^tail    ^head
 # [@@@------------------------------@@@@@@@@@] after first push over size
-#     ^start                        ^end
+#     ^head                         ^tail
 # [------------------------------------------] empty
-#                   ^start&end
+#                   ^head&tail
 # [@@@@@@@@@@@@@@@@-@@@@@@@@@@@@@@@@@@@@@@@@@] full
-#                  se
+#                  ht
 class RingBuffer:
     def __init__(self, size: int):
         if size < 2:
             raise ValueError("size must > 1")
         self.buf = bytearray(size)
-        self.start_pos = 0
-        self.end_pos = 0
+        self.head = 0
+        self.tail = 0
         self.size = size
 
     def __len__(self) -> int:
         return len(self.buf)
 
     def __repr__(self) -> str:
-        return f"{self.__class__.__name__}({bytes(self.buf)!r}, start={self.start_pos}, end={self.end_pos})"
+        return f"{self.__class__.__name__}({bytes(self.buf)!r}, head={self.head}, tail={self.tail})"
 
     @property
     def data_size(self) -> int:
-        if self.end_pos < self.start_pos:
-            return self.start_pos - self.end_pos
-        elif self.end_pos == self.start_pos:
+        if self.tail < self.head:
+            return self.head - self.tail
+        elif self.tail == self.head:
             return 0
         else:
-            return self.start_pos + self.size - self.end_pos
+            return self.head + self.size - self.tail
 
     @property
     def available_size(self) -> int:
@@ -51,15 +51,15 @@ class RingBuffer:
 
     @property
     def no_mcp_size(self) -> int:
-        if self.end_pos < self.start_pos:
-            return self.start_pos - self.end_pos
-        elif self.end_pos == self.start_pos:
+        if self.tail < self.head:
+            return self.head - self.tail
+        elif self.tail == self.head:
             return 0
         else:
-            return self.size - self.end_pos
+            return self.size - self.tail
 
     def clear(self) -> None:
-        self.start_pos = self.end_pos = 0
+        self.head = self.tail = 0
 
     def resize(self, size) -> None:
         if size < 2:
@@ -75,43 +75,40 @@ class RingBuffer:
 
     def pprint(self) -> None:
         print("\n", repr(self.buf), sep="")
-        if self.start_pos < self.end_pos:
-            len0 = len(repr(self.buf[: self.start_pos])) - 2
-            len1 = _chr_len(self.buf[self.start_pos])
-            len2 = len(repr(self.buf[self.start_pos + 1 : self.end_pos])) - 14
-            len3 = _chr_len(self.buf[self.end_pos])
+        if self.head < self.tail:
+            len0 = len(repr(self.buf[: self.head])) - 2
+            len1 = _chr_len(self.buf[self.head])
+            len2 = len(repr(self.buf[self.head + 1 : self.tail])) - 14
+            len3 = _chr_len(self.buf[self.tail])
             print(f"{' '*len0}{'s'*len1}{' '*len2}{'e'*len3}")
-        elif self.start_pos > self.end_pos:
-            len0 = len(repr(self.buf[: self.end_pos])) - 2
-            len1 = _chr_len(self.buf[self.end_pos])
-            len2 = len(repr(self.buf[self.end_pos + 1 : self.start_pos])) - 14
-            len3 = _chr_len(self.buf[self.start_pos])
+        elif self.head > self.tail:
+            len0 = len(repr(self.buf[: self.tail])) - 2
+            len1 = _chr_len(self.buf[self.tail])
+            len2 = len(repr(self.buf[self.tail + 1 : self.head])) - 14
+            len3 = _chr_len(self.buf[self.head])
             print(f"{' '*len0}{'s'*len1}{' '*len2}{'e'*len3}")
-        else:  # self.start_pos == self.end_pos:
-            len0 = len(repr(self.buf[: self.end_pos])) - 2
-            len1 = _chr_len(self.buf[self.end_pos])
+        else:  # self.head == self.tail:
+            len0 = len(repr(self.buf[: self.tail])) - 2
+            len1 = _chr_len(self.buf[self.tail])
             print(f"{' '*len0}{'^'*len1}")
 
     def is_full(self) -> bool:
-        return self.start_pos + 1 == self.end_pos
+        return self.head + 1 == self.tail
 
     def is_empty(self) -> bool:
-        return self.start_pos == self.end_pos
+        return self.head == self.tail
 
     def next(self) -> memoryview:
-        limit = (self.end_pos - 1) % self.size
-        if self.start_pos < limit:
-            return memoryview(self.buf)[self.start_pos : limit]
-        elif self.start_pos > limit:
-            return memoryview(self.buf)[self.start_pos :]
-        else:  # self.start_pos == limit
+        limit = (self.tail - 1) % self.size
+        if self.head < limit:
+            return memoryview(self.buf)[self.head : limit]
+        elif self.head > limit:
+            return memoryview(self.buf)[self.head :]
+        else:  # self.head == limit
             raise OverflowException
 
-    def _next2(self) -> memoryview:
-        return memoryview(self.buf)[: self.end_pos]
-
     def shift(self, nbytes: int) -> None:
-        self.start_pos = (self.start_pos + nbytes) % self.size
+        self.head = (self.head + nbytes) % self.size
 
     def push_bytes(self, data: bytes) -> None:
         length = len(data)
@@ -123,33 +120,33 @@ class RingBuffer:
             else:
                 shift_len = len(mv)
                 mv[:] = data[:shift_len]  # type: ignore
-                with memoryview(self.buf)[: self.end_pos] as mv2:
+                with memoryview(self.buf)[: self.tail] as mv2:
                     mv2[: length - shift_len] = data[shift_len:]  # type: ignore
         self.shift(length)
 
     def pull_bytes(self, nbytes: int) -> bytes:
         if nbytes < 1:
             raise ValueError("nbytes must >= 1")
-        if self.start_pos < self.end_pos:
-            if self.end_pos + nbytes <= self.size:
-                start = self.end_pos
-                self.end_pos = (self.end_pos + nbytes) % self.size
+        if self.head < self.tail:
+            if self.tail + nbytes <= self.size:
+                start = self.tail
+                self.tail = (self.tail + nbytes) % self.size
                 return bytes(memoryview(self.buf)[start:])
-            elif self.start_pos + self.size - self.end_pos >= nbytes:
-                start = self.end_pos
-                self.end_pos = start + nbytes - self.size
+            elif self.head + self.size - self.tail >= nbytes:
+                start = self.tail
+                self.tail = start + nbytes - self.size
                 with memoryview(self.buf) as mv:
-                    return b"".join((mv[start:], mv[: self.end_pos]))
+                    return b"".join((mv[start:], mv[: self.tail]))
             else:
                 raise OverflowException
-        elif self.start_pos > self.end_pos:
-            if self.end_pos + nbytes <= self.start_pos:
-                start = self.end_pos
-                self.end_pos += nbytes
-                return bytes(memoryview(self.buf)[start : self.end_pos])
+        elif self.head > self.tail:
+            if self.tail + nbytes <= self.head:
+                start = self.tail
+                self.tail += nbytes
+                return bytes(memoryview(self.buf)[start : self.tail])
             else:
                 raise OverflowException
-        else:  # self.start_pos == self.end_pos
+        else:  # self.head == self.tail
             raise OverflowException
 
 
